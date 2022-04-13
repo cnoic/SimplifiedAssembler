c7b3472bea0ef36758a720dc8e1742659ffe9cd1
NoicCrouzet <crouzet.noic10@gmail.com>
2022-04-13 18:45:02 +0200

    tetest

--- to_hex.py
+++ to_hex.py
@@ -35,14 +35,16 @@ def B(addr): # branch
 #w −> osef
 #p -> osef aussi
 def mem(p : bool, u : bool, w : bool, ldr : bool):# ldr / str
-    return 0b010 << 25 + p << 24 + u << 23 + w << 21 + ldr << 20
+    return (0b010 << 25) + (p << 24) + (u << 23) + (w << 21) + (ldr << 20)
 
 def accmem(rn, rt, imm): #ldr / str
-    return rn << 16 + rt << 12 + imm & 0xFFF
+    return (rn << 16) + (rt << 12) + (imm & 0xFFF)
 
 def calc(ope, store : bool, use_reg : bool, rn, rd, immrm):
-    return use_reg << 25 + ope << 21 + store << 20 + rn << 16 + rd << 12 + immrm & (0xF if use_reg else 0xFFF)
+    return (use_reg << 25) + (ope << 21) + (store << 20) + (rn << 16) + (rd << 12) + (immrm & (0xF if use_reg else 0xFFF))
 
+def condcond(cond):
+    return (cond << 28)
 
 #ADD RD,RS,RS
 #ADD RD,RS,#12
@@ -56,16 +58,20 @@ def calc(ope, store : bool, use_reg : bool, rn, rd, immrm):
 
 #B #addr
 
-f = open("demofile.txt", "r")
-for ligne in f:
+#f = open("demofile.txt", "r")
+#for ligne in f:
+while 1:
+    ligne = input()
     res = 0
     t = ligne.split(' ')
+    args = t[1].split(',')
     t[0] += "   "
-    if(t[0][:3] == "ADD") res = calc(ADD, t[0][3] == "S", not "#" in t[1],)
-    if(t[0][:3] == "SUB") res = calc()
-    if(t[0][:3] == "ORR") res = calc()
-    if(t[0][:3] == "AND") res = calc()
-    if(t[0][:3] == "CMP") res = calc()
-    if(t[0][:3] == "LDR") res = mem()
-    if(t[0][:3] == "STR") res = mem()
-    if(t[0][0] == "B") 
+    if(t[0][:3] == "ADD"): res = condcond(AL) + calc(ADD, t[0][3] == "S", args[2][0] =='R',int(args[1][1]),int(args[0][1]),int(args[2][1:]))
+    if(t[0][:3] == "SUB"): res = condcond(AL) + calc(SUB, t[0][3] == "S", args[2][0] =='R',int(args[1][1]),int(args[0][1]),int(args[2][1:]))
+    if(t[0][:3] == "ORR"): res = condcond(AL) + calc(ORR, t[0][3] == "S", args[2][0] =='R',int(args[1][1]),int(args[0][1]),int(args[2][1:]))
+    if(t[0][:3] == "AND"): res = condcond(AL) + calc(AND, t[0][3] == "S", args[2][0] =='R',int(args[1][1]),int(args[0][1]),int(args[2][1:]))
+    if(t[0][:3] == "CMP"): res = condcond(AL) + calc(CMP, t[0][3] == "S", args[2][0] =='R',int(args[1][1]),int(args[0][1]),int(args[2][1:]))
+    #if(t[0][:3] == "LDR") res = mem()
+    #if(t[0][:3] == "STR") res = mem()
+    #if(t[0][0] == "B") 
+    print(bytes(res))
